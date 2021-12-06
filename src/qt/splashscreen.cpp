f7f031b43039185ed959635294874353421dcf2c
anonymouszar <anonymouszarsa@gmail.com>
2021-12-06 16:20:03 +0200

    Set copyright and progress font colour to white

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -45,7 +45,7 @@ SplashScreen::SplashScreen(interfaces::Node& node, Qt::WindowFlags f, const Netw
     QSize splashSize(1154*devicePixelRatio,570*devicePixelRatio);
     pixmap = QPixmap(splashSize);
     QPainter pixPaint(&pixmap);
-    pixPaint.setPen(QColor(0,0,0));
+    pixPaint.setPen(QColor(255,255,255));
 
     // draw a slightly radial gradient
     QRect rectIcon(QPoint(0,0), QSize(1154,570));
@@ -122,7 +122,7 @@ static void InitMessage(SplashScreen *splash, const std::string &message)
         Qt::QueuedConnection,
         Q_ARG(QString, QString::fromStdString(message)),
         Q_ARG(int, Qt::AlignBottom|Qt::AlignHCenter),
-        Q_ARG(QColor, QColor(55,55,55)));
+        Q_ARG(QColor, QColor(255,255,255)));
 }
 
 static void ShowProgress(SplashScreen *splash, const std::string &title, int nProgress, bool resume_possible)
