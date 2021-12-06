d00a9b778865144fe715de03836aff99caf28f28
Doug H <41244792+Dhop14@users.noreply.github.com>
2021-12-06 16:24:40 -0500

    Fix spelling error

--- src/qt/networkstyle.cpp
+++ src/qt/networkstyle.cpp
@@ -27,7 +27,7 @@ NetworkStyle::NetworkStyle(const QString &_appName, const int iconColorHueShift,
     titleAddText(qApp->translate("SplashScreen", _titleAddText))
 {
     // load pixmap
-    QPixmap pixmap(":/icons/bitcion");
+    QPixmap pixmap(":/icons/bitcoin");
     QPixmap pixmapSplash(":/icons/splash");
 
     if(iconColorHueShift != 0 && iconColorSaturationReduction != 0)
